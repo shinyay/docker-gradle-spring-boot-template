7d14b576a03be33096c16406bdfdd8913543e458
shinyay <shinya.com@gmail.com>
2019-08-10 09:28:31 +0900

    Initial Commit

---
+++ src/main/kotlin/io/spring/shinyay/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package io.spring.shinyay.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
