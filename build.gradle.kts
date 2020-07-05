306b6345509450d58ba5655a1fb76e93870f6b72
shinyay <shinya.com@gmail.com>
2020-07-05 17:06:32 +0900

    Add Jacoco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "2.2.2.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	id("org.sonarqube") version "3.0"
+	id("jacoco")
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
 }
@@ -36,4 +37,16 @@ tasks {
 	bootJar {
 		archiveFileName.set("demo.jar")
 	}
+}
+
+jacoco {
+    toolVersion = "0.8.5"
+    reportsDir = file("$buildDir/customJacocoReportDir")
+}
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.destination = file("${buildDir}/jacocoHtml")
+    }
 }
